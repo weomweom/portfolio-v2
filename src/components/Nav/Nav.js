6de0d0aad8dfaf648d1da939b06b2207a99d9ebb
weomweom <16.homiaq.16@gmail.com>
2023-11-10 16:08:34 +0100

    improved nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -10,7 +10,7 @@ import './Nav.css';
 function Nav() {
   return (
     <>
-      <div className='w-full h-[--navHeight] bg-[--dark-purple] mono justify-end items-cetner fixed px-8 z-10 hidden md:flex'>
+      <div className='w-full h-[--navHeight] bg-[--dark-purple] mono justify-end items-cetner sticky top-0 px-8 z-10 hidden md:flex'>
           <ol className='h-full flex items-center justify-center flex-row'>
             {navLinks.map((item, i) => {
               return(
